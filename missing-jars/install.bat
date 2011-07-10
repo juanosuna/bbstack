232e984cfcab25eac3e8221f16ed2e5e2411fb51
Juan Osuna <juan@brownbagconsulting.com>
2011-07-11 00:14:03 +0200

    tweaks to get styles working in IE9

--- missing-jars/install.bat
+++ missing-jars/install.bat
@@ -1,1 +1,6 @@
+call mvn install:install-file -DgroupId=org.vaadin.addons -DartifactId=chameleon -Dversion=1.1.0 -Dpackaging=jar -Dfile=vaadin-chameleon-theme-1.1.0.jar
 call mvn install:install-file -DgroupId=org.vaadin.addons -DartifactId=chameleon -Dversion=1.0.2 -Dpackaging=jar -Dfile=vaadin-chameleon-theme-1.0.2.jar
+call mvn install:install-file -DgroupId=org.vaadin.addons -DartifactId=chameleon-blue -Dversion=1.0.2 -Dpackaging=jar -Dfile=chameleon-blue.jar
+call mvn install:install-file -DgroupId=org.vaadin.addons -DartifactId=chameleon-dark -Dversion=1.0.2 -Dpackaging=jar -Dfile=chameleon-dark.jar
+call mvn install:install-file -DgroupId=org.vaadin.addons -DartifactId=chameleon-green -Dversion=1.0.2 -Dpackaging=jar -Dfile=chameleon-green.jar
+call mvn install:install-file -DgroupId=org.vaadin.addons -DartifactId=chameleon-vaadin -Dversion=1.0.2 -Dpackaging=jar -Dfile=chameleon-vaadin.jar
